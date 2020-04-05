7ba01c44347418837a76c4d715cb1d376b08eac7
jakubbrzostek <brzostekjakub@gmail.com>
2020-04-06 01:07:05 +0200

    still lots to do

--- src/net/codestrikes/PlayerBot.java
+++ src/net/codestrikes/PlayerBot.java
@@ -6,22 +6,40 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.Map.entry;
+
 public class PlayerBot extends BotBase {
-    private Area defence = Area.UppercutPunch;
-    private int myLifeLeft = 0;
-    private int opponentsLifeLeft = 0;
     private int myScoreTotal = 0;
     private int opponentScoreTotal = 0;
-    private int roundCounter = 100;
+//    private int roundCounter = 100;
 
-    public List<RoundContext> opponentsMoves = new ArrayList<RoundContext>();
+    private Map<Area, Integer> attackEnergyMap = Map.ofEntries(
+            entry(Area.HookKick, 4),
+            entry(Area.HookPunch, 3),
+            entry(Area.UppercutPunch, 2),
+            entry(Area.LowKick, 1)
+    );
 
-    private Area changeDefence(Area oldDefence) {
-        return (oldDefence == Area.HookKick) ? Area.HookPunch : Area.HookKick;
-    }
+    private Map<Area, Integer> defenceEnergyMap = Map.ofEntries(
+            entry(Area.HookKick, 4),
+            entry(Area.HookPunch, 4),
+            entry(Area.UppercutPunch, 4),
+            entry(Area.LowKick, 4)
+    );
+
+
+    private List<RoundContext> opponentsMoves = new ArrayList<RoundContext>();
+
+//    private Area changeDefence(Area oldDefence) {
+//        return (oldDefence == Area.HookKick) ? Area.HookPunch : Area.HookKick;
+//    }
 
     private Area createRandomAttack() {
         return new Random().nextDouble() > 0.5d ? Area.LowKick : Area.HookPunch;
+    }
+
+    private void changeCurrentContext(RoundContext context, Area deleteElement, Area addElement) {
+
     }
 
     private void createAggressiveAttack(RoundContext context) {
@@ -29,10 +47,26 @@ public class PlayerBot extends BotBase {
         context.getMyMoves()
                 .addAttack(Area.HookKick)
                 .addAttack(Area.HookPunch)
+                .addAttack(Area.UppercutPunch)
                 .addAttack(Area.LowKick)
                 .addAttack(Area.LowKick)
-                .addAttack(Area.LowKick)
-                .addAttack(Area.UppercutPunch);
+                .addAttack(Area.LowKick);
+    }
+
+    private void createTotalAttack(RoundContext context) {
+
+        context.getMyMoves()
+                .addAttack(Area.HookKick)
+                .addAttack(Area.HookKick)
+                .addAttack(Area.HookKick);
+    }
+
+    private void createFullDefence(RoundContext context) {
+
+        context.getMyMoves()
+                .addDefence(Area.HookKick)
+                .addDefence(Area.HookPunch)
+                .addDefence(Area.UppercutPunch);
     }
 
     private Area findMovesByAttackAnalyze() {
@@ -47,7 +81,7 @@ public class PlayerBot extends BotBase {
         }
 
         mostPopularMove = (tmpList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream().min(Comparator.comparing(Map.Entry::getValue)).orElse(null));
+                .entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).orElse(null));
 
         return Objects.requireNonNull(mostPopularMove).getKey().getArea();
     }
@@ -64,24 +98,27 @@ public class PlayerBot extends BotBase {
         }
 
         mostPopularMove = (tmpList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).orElse(null));
+                .entrySet().stream().min(Comparator.comparing(Map.Entry::getValue)).orElse(null));
 
         return Objects.requireNonNull(mostPopularMove).getKey().getArea();
     }
 
     public MoveCollection nextMove(RoundContext context) {
-        myLifeLeft = context.getMyLifePoints();
-        opponentsLifeLeft = context.getOpponentLifePoints();
+        int myLifeLeft = context.getMyLifePoints();
+        int opponentsLifeLeft = context.getOpponentLifePoints();
         myScoreTotal += context.getMyDamage();
         opponentScoreTotal += context.getOpponentDamage();
 
-        if (opponentsMoves.size() < 99) {
-            createAggressiveAttack(context);
-        } else if (myScoreTotal >= opponentScoreTotal) {
-            context.getMyMoves().addAttack(createRandomAttack()); // 3 attacks, 0 defence
+        if (myLifeLeft > opponentsLifeLeft) {
+            if (Arrays.stream(context.getLastOpponentMoves().getDefences()).noneMatch(i -> i.getArea() == Area.HookKick)) {
+                createTotalAttack(context);
+            } else {
+                createAggressiveAttack(context);
+            }
 
             //If my defence don't contains most popular opponent attack, change my defence
         } else if (!context.getMyMoves().hasDefence(findMovesByAttackAnalyze())) {
+            Area defence = Area.UppercutPunch;
             if (context.getMyMoves().getDefences() == null) {
                 defence = findMovesByAttackAnalyze();
                 context.getMyMoves().addDefence(defence);
@@ -90,12 +127,12 @@ public class PlayerBot extends BotBase {
             }
         }
         //If my attack don't contains least popular opponent defence, change my attack
-        else if (Arrays.stream(context.getMyMoves().getAttacks()).noneMatch(i -> i.getArea() == findMovesByDefenceAnalyze())){
+        else if (Arrays.stream(context.getMyMoves().getAttacks()).noneMatch(i -> i.getArea() == findMovesByDefenceAnalyze())) {
 
         }
 
         // Only if I'll won by blocking everything besides LowKicks
-        else if (myLifeLeft - opponentsMoves.size() > opponentsLifeLeft) {
+        else if (myLifeLeft - opponentsMoves.size() * 12 > opponentsLifeLeft) {
             context.getMyMoves()
                     .addDefence(Area.HookKick)
                     .addDefence(Area.HookPunch)
